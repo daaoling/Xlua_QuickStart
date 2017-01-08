6ea2341b0bc5b17bc9004e31abc99d2fdac00dbd
daaoling <1294246096@qq.com>
2017-01-08 21:03:45 +0800

    Assets\Exercise 新增练习代码

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #xlua 学习案例
 
+
+2017-1-8
+
+Assets\Exercise 新增练习代码
+
+
+
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add --all
-git commit -m "上传MVC例子" 
+git commit -m "Assets\Exercise 新增练习代码" 
 git push github master
